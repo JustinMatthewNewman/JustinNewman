fc3c9b636d3378dfa88b62a40bd32cb31cbabd06
Justin Newman <justin.newman@metalinguist.com>
2023-07-04 15:46:56 -0400

    new projects cards

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,48 +1,70 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
-import melodifyImg from '../public/assets/projects/melod.png';
-import jmudevImg from '../public/assets/projects/jmudev.png';
-import cscodelogoImg from '../public/assets/projects/cscode.png';
-import automatedAthleticImg from '../public/assets/projects/code.png';
-import ProjectItem from './ProjectItem';
+import { Card, Grid, Row, Text } from "@nextui-org/react";
+import Link from 'next/link';
 
 const Projects = () => {
+  const projects = [
+    {
+      title: 'MelodifyLabs.com',
+      img: '/assets/projects/melod.png',
+      projectUrl: 'https://melodifylabs.com',
+      tech: 'Next.js, Firebase, Vercel'
+    },
+    {
+      title: 'JmuDev.com',
+      img: '/assets/projects/jmudev.png',
+      projectUrl: 'https://w3stu.cs.jmu.edu/newma4jm/website/index.html',
+      tech: 'HTML, CSS, PHP'
+    },
+    {
+      title: 'CSCode.org',
+      img: '/assets/projects/cscode.png',
+      projectUrl: 'https://cscode.org',
+      tech: 'React, NextAuth, Tailwind CSS'
+    },
+    {
+      title: 'Automated Athletic Statistics Software',
+      img: '/assets/projects/code.png',
+      projectUrl: 'https://github.com/JustinMatthewNewman/Stars',
+      tech: 'Java, Jsoup'
+    },
+
+
+  ];
+
   return (
-    <div id='projects' className='w-full'>
-      <div className='max-w-[1240px] mx-auto px-2 py-16'>
-        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
-          Solutions
-        </p>
-        <h2 className='py-4'>I&apos;ve developed.</h2>
-        <div className='grid md:grid-cols-2 gap-8'>
-          <ProjectItem
-            title='MelodifyLabs.com'
-            backgroundImg={melodifyImg}
-            projectUrl='https://melodifylabs.com'
-            tech='Next.js, Firebase, Vercel'
-          />
-          <ProjectItem
-            title='JmuDev.com'
-            backgroundImg={jmudevImg}
-            projectUrl='https://w3stu.cs.jmu.edu/newma4jm/website/index.html'
-            tech='HTML, CSS, PHP'
-          />
-          <ProjectItem
-            title='CSCode.org'
-            backgroundImg={cscodelogoImg}
-            projectUrl='https://cscode.org'
-            tech='React, NextAuth, Tailwind CSS'
-          />
-          <ProjectItem
-            title='Automated Athletic Statistics Software'
-            backgroundImg={automatedAthleticImg}
-            projectUrl='https://github.com/JustinMatthewNewman/Stars'
-            tech='Java, Jsoup'
-          />
-        </div>
-      </div>
-    </div>
+    <Grid.Container gap={4} justify="flex-start" id='projects'>
+      {projects.map((project, index) => (
+        <Grid xs={6} sm={3} key={index}>
+          <Card isPressable css={{
+              transition: 'transform 0.3s ease-in-out', 
+              '&:hover': { transform: 'scale(1.05)' } 
+          }}>
+            <a href={project.projectUrl} target='_blank' rel='noreferrer'>
+              <Card.Body css={{ p: 0 }}>
+                <Card.Image
+                  src={project.img}
+                  objectFit="cover"
+                  width="100%"
+                  height={140}
+                  alt={project.title}
+                />
+              </Card.Body>
+              <Card.Footer css={{ justifyItems: "flex-start" }}>
+                <Row wrap="wrap" justify="space-between" align="center">
+                  <Text b>{project.title}</Text>
+
+                  <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
+                    {project.tech}
+                  </Text>
+                </Row>
+              </Card.Footer>
+            </a>
+          </Card>
+        </Grid>
+      ))}
+    </Grid.Container>
+
   );
 };
 
