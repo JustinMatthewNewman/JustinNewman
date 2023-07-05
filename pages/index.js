dc889c092b6856c174f7b3aa2de3ca81cfb42a02
Justin Newman <justin.newman@metalinguist.com>
2023-07-04 22:11:20 -0400

    optimization

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
-import About from '../components/About';
-import Contact from '../components/Contact';
-import Main from '../components/Main';
-import Projects from '../components/Projects';
-import Skills from '../components/Skills';
+import Head from 'next/head'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Main from '../components/Main'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate a delay for demonstration purposes
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []);
-
   return (
     <div>
       <Head>
@@ -23,24 +14,17 @@ const Home = () => {
         <meta name="description" content="I’m a human." />
         <link rel="icon" href="/fav.png" />
       </Head>
-      {isLoading ? (
-        // Loading spinner or placeholder component
-        <div className="loading-spinner">
-          <span>Loading...</span>
-        </div>
-      ) : (
-        <>
-          <Main />
-          <div style={{ position: 'relative', zIndex: 1 }}>
-            <About />
-            <Skills />
-            <Projects />
-            <Contact />
-          </div>
-        </>
-      )}
+      <Main />
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
     </div>
   );
 };
 
 export default Home;
+
