dc38a4d3e0a1e144fb63bb1d1896db12999f5a7f
Justin Newman <justin.newman@metalinguist.com>
2023-07-07 13:15:49 -0400

    Auto typer

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 
 const FloatingText = React.memo(() => {
-  const [hovered, setHovered] = useState(false);
 
   const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
-    transform: `translate(-50%, -50%) scale(${hovered ? 1.2 : 1})`,
+    transform: `translate(-50%, -50%)`,
     transition: 'transform 0.3s',
     fontSize: '2em',
-    cursor: 'pointer',
     zIndex: 3,
     animation: 'fade-in 4s'  // 2s is the duration of the animation
   };
@@ -19,10 +18,22 @@ const FloatingText = React.memo(() => {
   return (
     <div
       style={style}
-      onMouseOver={() => setHovered(true)}
-      onMouseOut={() => setHovered(false)}
     >
-      Hi, I&apos;m Justin.
+      <TypeAnimation
+  sequence={[
+    // Same substring at the start will only be typed once, initially
+    'Hi, welcome to my website!',
+    1000,
+    'My name is Justin Newman.',
+    1000,
+    'I would love to hear from you!',
+    1000,
+    
+  ]}
+  speed={50}
+  style={{ fontSize: '1em' }}
+  repeat={Infinity}
+/>
     </div>
   );
 });
