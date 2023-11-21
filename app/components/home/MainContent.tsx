3477152da11817c3437998e3049925739b518e76
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2023-11-21 10:35:09 -0500

    Updated photo

--- app/components/home/MainContent.tsx
+++ app/components/home/MainContent.tsx
@@ -24,10 +24,8 @@ export default function MainContent() {
       width={600}
     />
     <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-      <p className="text-tiny text-white/80">#OpenToWork</p>
-      <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-        Email me
-      </Button>
+      <p className="text-tiny text-white/80">#ComputerScience</p>
+      <p className="text-tiny text-white/80">#DataAnalysis</p>
     </CardFooter>
   </Card>
 
